c30ed1a9f7435601e44a335f73f0915d033d97c5
Amber Aultman <amber.aultman@gmail.com>
2017-08-29 22:40:18 -0400

    Assignment 25

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,21 @@ var albumPicasso = {
      ]
  };
 
+ var albumAmber = {
+     title: 'Hey, hey, hey',
+     artist: 'Amber Aultman',
+     label: 'Slamber Productions',
+     year: '1980',
+     albumArtUrl: 'assets/images/album_covers/18.png',
+     songs: [
+         { title: 'Baby, I got your money', duration: '1:01' },
+         { title: 'Killin\' it', duration: '5:01' },
+         { title: 'All the things', duration: '3:21'},
+         { title: 'We the AIGA?', duration: '3:14' },
+         { title: 'Kicking ass, taking names', duration: '2:15'}
+     ]
+ };
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -40,15 +55,14 @@ var albumPicasso = {
      return template;
  };
 
- var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-     // #2
+ var setCurrentAlbum = function(album) {
+
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -65,4 +79,15 @@ var albumPicasso = {
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     var albums = [albumPicasso, albumMarconi, albumAmber];
+     var index = 1;
+
+     albumImage.addEventListener( "click" , function(event) {
+       setCurrentAlbum(albums[index]);
+       index++;
+       if( index == albums.length ){
+         index = 0;
+       }
+     });
  };
